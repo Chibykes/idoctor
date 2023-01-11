b075d621e2e598a07c4f71d24aa785a3792f8bba
Chukwubuikem Chiabuotu <chibykes99@gmail.com>
2023-01-11 16:54:30 +0530

    Remove junk code

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
 	render() {
@@ -15,10 +14,6 @@ class MyDocument extends Document {
 				<body className="">
 					<Main />
 					<NextScript />
-					<Script
-						src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&callback=initMap&v=weekly&libraries=places`}
-						strategy='beforeInteractive'
-					/>
 				</body>
 			</Html>
 		);
